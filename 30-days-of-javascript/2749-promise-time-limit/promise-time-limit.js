fbba5d749952f8e7ce1b2daed3d4608bfea12cd2
Asif Azam Biswas <96988265+asif-852@users.noreply.github.com>
2026-02-01 22:43:29 +0600

    Time: 42 ms (90.72%) | Memory: 54.4 MB (43.83%) - LeetSync

---
+++ 30-days-of-javascript/2749-promise-time-limit/promise-time-limit.js
@@ -0,0 +1,24 @@
+/**
+ * @param {Function} fn
+ * @param {number} t
+ * @return {Function}
+ */
+var timeLimit = function(fn, t) {
+
+    return async function(...args) {
+        const timeoutPromise = new Promise((_, reject) => {
+            setTimeout(() => {
+                reject("Time Limit Exceeded");
+            }, t);
+        });
+        const functionPromise = fn(...args);
+
+        // Racing between 2 promises
+        return Promise.race([functionPromise, timeoutPromise]);
+    }
+};
+
+/**
+ * const limited = timeLimit((t) => new Promise(res => setTimeout(res, t)), 100);
+ * limited(150).catch(console.log) // "Time Limit Exceeded" at t=100ms
+ */
