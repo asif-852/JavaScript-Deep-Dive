62dde6f7ccdeacc4f19fbc8eb3fe8a6d477d393f
Asif Azam Biswas <96988265+asif-852@users.noreply.github.com>
2026-02-16 22:29:59 +0600

    Time: 99 ms (79.71%) | Memory: 79 MB (55.70%) - LeetSync

---
+++ 30-days-of-javascript/2742-group-by/group-by.js
@@ -0,0 +1,18 @@
+/**
+ * @param {Function} fn
+ * @return {Object}
+ */
+Array.prototype.groupBy = function(fn) {
+    const res = {};
+    for(ele of this) {
+        const key = fn(ele);
+        res[key] = res[key] ?? [];  // Nullish Coalescing Operator (??)
+        res[key].push(ele);
+    }
+    return res;
+};
+
+
+/**
+ * [1,2,3].groupBy(String) // {"1":[1],"2":[2],"3":[3]}
+ */
