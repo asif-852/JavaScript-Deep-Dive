882dbda40dd2e3d626da04831b9138a2cfe38b3e
Asif Azam Biswas <96988265+asif-852@users.noreply.github.com>
2026-02-17 23:30:22 +0600

    Time: 260 ms (84.59%) | Memory: 102.4 MB (52.23%) - LeetSync

---
+++ 30-days-of-javascript/2858-join-two-arrays-by-id/join-two-arrays-by-id.js
@@ -0,0 +1,25 @@
+/**
+ * @param {Array} arr1
+ * @param {Array} arr2
+ * @return {Array}
+ */
+var join = function(arr1, arr2) {
+    const res = {};
+    const used = new Map();
+    for(obj of arr1) {
+        used.set(obj.id, 1);
+        res[obj.id] = obj;
+    }
+    for(let i = 0; i < arr2.length; i++) {
+        const key = arr2[i].id;
+        if(used.has(key)) {
+            for(k in arr2[i]) {
+                res[key][k] = arr2[i][k];
+            }
+        } else {
+            res[key] = arr2[i];
+        }
+    }
+
+    return Object.values(res);
+};
